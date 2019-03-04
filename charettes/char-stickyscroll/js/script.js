33bc606f24cfc1adbdd646cb817f4fd277c4a621
maggierc <31423327+maggierc@users.noreply.github.com>
2019-03-03 19:47:13 -0600

    continuing louis, trying new layout

---
+++ charettes/char-stickyscroll/js/script.js
@@ -0,0 +1,58 @@
+// Options documentation at:
+// https://github.com/alvarotrigo/fullPage.js
+
+// opacity/display none, then opacity/display block
+// have consistent color
+
+$(document).ready(function() {
+
+  // var playFilter = false;
+  //
+  // $("#readplayButton").toggle(function()  {
+  //       $('#masked-page').removeClass("ClipOn").addClass("ClipOff");
+  //     },
+  //     function() {
+  //         $('#masked-page').removeClass("ClipOff").addClass("ClipOn");
+  //     });
+
+
+
+    $("#readplayButton").toggle(function()  {
+          $('#masked-page').removeClass("ClipOn").addClass("ClipOff");
+          setTimeout(function(){
+            $('.readText').css("display", "none");
+            $('.playText').css("display", "inherit");
+            }, 1000);
+          playFilter = true;
+
+          },
+
+          function() {
+            $('#masked-page').removeClass("ClipOff").addClass("ClipOn");
+            setTimeout(function(){
+              $('.playText').css("display", "none");
+              $('.readText').css("display", "inherit");
+              }, 1000);
+            playFilter = false;
+    });
+
+
+
+    // $('p.playText').mouseover(function(){
+    //
+    //   if (playFilter==true) {
+    //     $(this).text("tan");
+    //     $(this).css("color", "yellow");
+    //     $(this).css("font-size", "2em");
+      // }
+    //   else {
+    //   }
+    //   // $(this).text("tan");
+    //   // $(this).css("color", "red");
+    //
+    // });
+
+
+
+
+});
