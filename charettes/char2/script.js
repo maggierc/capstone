be36d47068c4be7e05f761afcfed1806d33ce58e
maggierc <31423327+maggierc@users.noreply.github.com>
2019-03-24 11:48:27 -0500

    charettes

--- charettes/char2/script.js
+++ charettes/char2/script.js
@@ -1,38 +1,11 @@
-// Options documentation at:
-// https://github.com/alvarotrigo/fullPage.js
-
-// opacity/display none, then opacity/display block
-// have consistent color
-
 $(document).ready(function() {
 
-  var playFilter = false;
-
-    // $('#fullpage').fullpage({
-    //   navigation: true,
-    // });
-
-    $("#readplayButton0").toggle(function() {
-          $('#masked-page').removeClass("ClipOn").addClass("ClipOff");
-
-          setTimeout(function(){
-            $('.readText').css("display", "none");
-            $('.playText').css("display", "inherit");
-            }, 1000);
-
-            playFilter = true;
-
-          }, function() {
-            $('#masked-page').removeClass("ClipOff").addClass("ClipOn");
-
-            setTimeout(function(){
-              $('.playText').css("display", "none");
-              $('.readText').css("display", "inherit");
-            }, 500);
+  $('#reload').click(function() {
+    location.reload();
+  });
 
 
-            playFilter = false;
-    });
+  var playFilter = true;
 
     $('p.playText').each(function() {
 
@@ -52,7 +25,7 @@ $(document).ready(function() {
 
       });
 
-    $('.playText span, .playFX').mouseover(function(){
+    $('#section1 .playText span').mouseover(function(){
 
       var random = (Math.random() * 2.5) + 1;
 
@@ -63,7 +36,7 @@ $(document).ready(function() {
             // 'display': 'inline-block',
             'float' : 'left',
             'text-transform' : 'uppercase',
-            'line-height' : '0.7em'
+            'line-height' : '0.7em',
         })
 
       }
@@ -72,30 +45,57 @@ $(document).ready(function() {
 
     });
 
-    //
-    // function myFunction() {
-    //   setInterval(function(){
-    //     $(".playTextPreview").css('color','yellow');
-    //   }, 1000);
-    // }
-    //
-    // myFunction();
+    $('#section2 .playText span').mouseenter(function() {
+      var text = this.innerText;
+      $(this).css({'color':'cyan', 'opacity':'1'});
+      var divW = (window.innerWidth / 10).toFixed();
+      var divH = (window.innerHeight / 10).toFixed();
+      var value = Math.random() * 0xFF | 0;
+      var grayscale = (value << 16) | (value << 8) | value;
+      var color = '#' + grayscale.toString(16);
+      // var color = '#' + Math.round(0xffffff * Math.random()).toString(16);
+      $newdiv = $('<h3/>').css({
+          'color': color,
+      });
+
+      var posx = (Math.random() * ($(document).innerWidth() - divW)).toFixed();
+      var posy = (Math.random() * ($(document).innerHeight() - divH)).toFixed();
+
+      $newdiv.css({
+          'position': 'absolute',
+          'left': posx + 'px',
+          'top': posy + 'px',
+          'display': 'none',
+          'filter': 'blur(0.2px)',
+          'opacity' : '0.1'
+      }).text(text).appendTo('body').fadeIn(500).delay(500).fadeOut(500);
+    });
+
+    $('#section2 .playText span').mouseleave(function() {
+      var text = this.innerText;
+      $(this).css('opacity', '0');
+      var divW = (window.innerWidth / 10).toFixed();
+      var divH = (window.innerHeight / 10).toFixed();
+      var value = Math.random() * 0xFF | 0;
+      var grayscale = (value << 16) | (value << 8) | value;
+      var color = '#' + grayscale.toString(16);
+      // var color = '#' + Math.round(0xffffff * Math.random()).toString(16);
+      $newdiv = $('<h1/>').css({
+          'color': color,
+      });
+
+      var posx = (Math.random() * ($(document).innerWidth() - divW)).toFixed();
+      var posy = (Math.random() * ($(document).innerHeight() - divH)).toFixed();
+
+      $newdiv.css({
+          'position': 'absolute',
+          'left': posx + 'px',
+          'top': posy + 'px',
+          'filter': 'blur(1px)',
+          'display': 'none'
+      }).text(text).appendTo('body').fadeIn(500).delay(500).fadeOut(500);
+    });
 
 
-    function loop() {
-      setInterval(function(){
-          $("#playTextPreview").css('color', 'yellow');
-          $("#playTextPreview").text('TAN');
-          setInterval(function(){
-              $("#playTextPreview").css('color', 'white');
-              $("#playTextPreview").text("e");
-            },1500);
-        },2000);
-        loop();
 
-    };
-
-    loop();
-
-
-});
+  });
