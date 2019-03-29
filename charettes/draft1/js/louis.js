b62a864728d6ce8e8e59ce3fbb2fb21466ee0d78
maggierc <31423327+maggierc@users.noreply.github.com>
2019-03-29 15:40:34 -0500

    new imgs

--- charettes/draft1/js/louis.js
+++ charettes/draft1/js/louis.js
@@ -8,6 +8,44 @@ $(document).ready(function() {
 
   var playFilter = false;
 
+  $("#readplayButton0").mouseenter(function(){
+    $('#readplayButton3').css({
+        'height': '60px',
+        'width': '60px',
+        'background' : 'white'
+    });
+  });
+
+  $("#readplayButton0").mouseleave(function(){
+    $('#readplayButton3').css({
+        'height': '20px',
+        'width': '20px',
+        'background' : 'none'
+    });
+  });
+
+    $("#readplayButton0").toggle(function() {
+          $('#masked-page').removeClass("ClipOn").addClass("ClipOff");
+
+          setTimeout(function(){
+            $('.readText').css("display", "none");
+            $('.playText').css("display", "inherit");
+            }, 1000);
+
+            playFilter = true;
+
+          }, function() {
+            $('#masked-page').removeClass("ClipOff").addClass("ClipOn");
+
+            setTimeout(function(){
+              $('.playText').css("display", "none");
+              $('.readText').css("display", "inherit");
+            }, 500);
+
+
+            playFilter = false;
+    });
+
     $('p.playText').each(function() {
 
         var words = $(this).text().split(' ');
@@ -28,13 +66,14 @@ $(document).ready(function() {
 
     $('.playText span, .playFX').mouseover(function(){
 
-      var random = (Math.random() * 2.5) + 1;
+      var random = (Math.random() * 3) + 0.5;
 
       if (playFilter==true) {
         $(this).text("tan").css({
             'color': '#f6ff00',
             'font-size': random + 'em',
-            // 'display': 'inline-block',
+            'font-family' : 'Roboto',
+            'font-weight' : '500',
             'float' : 'left',
             'text-transform' : 'uppercase',
             'line-height' : '0.7em'
