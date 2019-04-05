8328230b91ee56e1136feeffc1d3beda72d7e227
maggierc <31423327+maggierc@users.noreply.github.com>
2019-04-05 14:35:02 -0500

    cleaning up gallery

--- charettes/draft1/gallery.js
+++ charettes/draft1/gallery.js
@@ -5,14 +5,14 @@ $( document ).ready(function() {
       $(".overlay").css("background", "#0e2844");
       $("#cliveOverlay").css("background", "none");
       $('body').css("background", "#0e2844");
-      $('#cliveImg').css("box-shadow", "0px 0px 20px rgba(255, 255, 255, 0.2)");
        $('#textReplace').css("color", "#fff").text( "Clive Wearing" );
+       $('#textReplaceType').css("color", "#fff").text( "Memory" );
     })
     .mouseleave(function() {
       $(".overlay").css("background", "none");
       $('body').css("background", "#eee");
-      $('#cliveImg').css("box-shadow", "none");
       $('#textReplace').css("color", "#222").text( "Select a patient to see their story" );
+      $('#textReplaceType').css("color", "#fff").text( "" );
     });
 
   $( "#phineasGallery .overlay" )
@@ -20,14 +20,14 @@ $( document ).ready(function() {
       $(".overlay").css("background", "#6d0021");
       $("#phineasOverlay").css("background", "none");
       $('body').css("background", "#6d0021");
-      $('#phineasImg').css("box-shadow", "0px 0px 20px rgba(255, 255, 255, 0.2)");
       $('#textReplace').css("color", "#fff").text( "Phineas Gage" );
+      $('#textReplaceType').css("color", "#fff").text( "Personality" );
     })
     .mouseleave(function() {
       $(".overlay").css("background", "none");
       $('body').css("background", "#eee");
-      $('#phineasImg').css("box-shadow", "none");
       $('#textReplace').css("color", "#222").text( "Select a patient to see their story" );
+      $('#textReplaceType').css("color", "#fff").text( "" );
     });
 
   $( "#louisGallery .overlay" )
@@ -35,14 +35,14 @@ $( document ).ready(function() {
       $(".overlay").css("background", "#36094f");
       $("#louisOverlay").css("background", "none");
       $('body').css("background", "#36094f");
-      $('#louisImg').css("box-shadow", "0px 0px 20px rgba(255, 255, 255, 0.2)");
-      $('#textReplace').css("color", "#fff").text( "Louis Victor Leborgne" );
+      $('#textReplace').css("color", "#fff").text( "Louis Leborgne" );
+      $('#textReplaceType').css("color", "#fff").text( "Language" );
     })
     .mouseleave(function() {
       $(".overlay").css("background", "none");
       $('body').css("background", "#eee");
-      $('#louisImg').css("box-shadow", "none");
       $('#textReplace').css("color", "#222").text( "Select a patient to see their story" );
+      $('#textReplaceType').css("color", "#fff").text( "" );
     });
 
   $( "#kimGallery .overlay" )
@@ -50,15 +50,14 @@ $( document ).ready(function() {
       $(".overlay").css("background", "#6d2a00");
       $("#kimOverlay").css("background", "none");
       $('body').css("background", "#6d2a00");
-      $('#kimImg').css("box-shadow", "0px 0px 20px rgba(255, 255, 255, 0.2)");
       $('#textReplace').css("color", "#fff").text( "Kim Peek" );
-
+      $('#textReplaceType').css("color", "#fff").text( "Knowledge" );
     })
     .mouseleave(function() {
       $(".overlay").css("background", "none");
       $('body').css("background", "#eee");
-      $('#kimImg').css("box-shadow", "none");
       $('#textReplace').css("color", "#222").text( "Select a patient to see their story" );
+      $('#textReplaceType').css("color", "#fff").text( "" );
     });
 
 
